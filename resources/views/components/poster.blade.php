a951157a5db9cbbd1e2fdc51add9cf8f89632119
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2025-05-13 17:43:22 +0100

    Create poster component

---
+++ resources/views/components/poster.blade.php
@@ -0,0 +1,17 @@
+@props(['title', 'release_year', 'score', 'duration', 'picture'])
+
+<div class="w-80 min-w-56 rounded-xl shadow-lg bg-black">
+    <img src="{{ $picture }}" alt="{{ $title }}" class="w-full h-60 object-cover"/>
+
+    <!-- Text content at bottom -->
+    <div class="bottom-0 p-3 text-white text-sm">
+        <h3 class="font-semibold leading-tight">{{ $title }}</h3>
+        <div class="flex items-center gap-2 text-xs mt-1 text-gray-300">
+            <span>{{ $release_year }}</span>
+            <span>•</span>
+            <span>⭐ {{ $score }}</span>
+            <span>•</span>
+            <span>{{ $duration }} min</span>
+        </div>
+    </div>
+</div>
