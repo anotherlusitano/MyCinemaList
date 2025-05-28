096f2da7b70e9a5d6b800b053430012623700ea6
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2025-05-28 14:32:05 +0100

    Add filters when searching for Movies

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -22,8 +22,13 @@ class SearchController extends Controller
                     'people' => $this->filterPeople($query, $sort)->paginate(8),
                 ]);
             case 'movies':
+                $sort = $request->query('sort') ?? 'title|asc';
+                $status = $request->query('status') ?? '';
+                $genre = $request->query('genre') ?? '';
+                $rating = $request->query('rating') ?? '';
+
                 return view('search.movies', [
-                    'movies' => $this->filterMovies($query)->paginate(8),
+                    'movies' => $this->filterMovies($query, $sort, $status, $rating, $genre)->paginate(8),
                 ]);
             case 'users':
                 $sort_alpha = $request->query('sort_alpha') ?? 'asc';
@@ -60,11 +65,25 @@ class SearchController extends Controller
             ->orderBy('created_at', $sort_date);
     }
 
-    public function filterMovies(string $name)
+    public function filterMovies(string $name, string $sort = "title|asc", string $status = '', string $rating = '', string $genre = '')
     {
+        [$field, $direction] = explode('|', $sort);
+
         return Movie::query()->where(function ($query) use ($name) {
             $query->where('title', 'like', "%$name%");
             $query->orWhere('synopsis', 'like', "%$name%");
-        });
+        })
+            ->when($status !== '', function ($query) use ($status) {
+                $query->where('status', $status);
+            })
+            ->when($rating !== '', function ($query) use ($rating) {
+                $query->where('rating', $rating);
+            })
+            ->when($genre !== '', function ($query) use ($genre) {
+                $query->whereHas('genres', function ($query) use ($genre) {
+                    $query->where('name', $genre);
+                });
+            })
+            ->orderBy($field, $direction);
     }
 }
--- resources/views/search/movies.blade.php
+++ resources/views/search/movies.blade.php
@@ -1,7 +1,53 @@
 <x-app-layout>
     <div class="flex flex-row min-h-4/5 w-full mt-6">
-        <div class="flex flex-col items-center max-w-lg w-1/4">
-        </div>
+        <form method="GET" action="{{ route('search') }}"
+              class="flex flex-col px-10 max-w-xs w-1/4"
+        >
+            <input type="hidden" name="type" value="movies">
+            <div class="flex bg-gray-800 mb-6">
+                <input
+                    type="text"
+                    name="query"
+                    value="{{ request('query') }}"
+                    placeholder="Pesquise..."
+                    class="bg-transparent text-sm text-white placeholder-gray-400 focus:outline-none w-48"
+                />
+                <button type="submit"
+                        class="flex justify-center items-center text-white w-full focus:outline-none">
+                    <x-tabler-search/>
+                </button>
+            </div>
+            <select name="sort" class="mb-4">
+                <option value="title|asc" {{ request('sort') == 'title|asc' ? 'selected' : '' }}>A-z</option>
+                <option value="title|desc" {{ request('sort') == 'title|desc' ? 'selected' : '' }}>Z-a
+                </option>
+                <option value="release_year|desc" {{ request('sort') == 'release_year|desc' ? 'selected' : '' }}>Recent
+                </option>
+                <option value="release_year|asc" {{ request('sort') == 'release_year|asc' ? 'selected' : '' }}>Oldest
+                </option>
+            </select>
+            <select name="status" class="mb-4">
+                <option value="" {{ request('status') == '' ? 'selected' : '' }}>All Status</option>
+                <option value="released" {{ request('status') == 'released' ? 'selected' : '' }}>Released</option>
+                <option value="unreleased" {{ request('status') == 'unreleased' ? 'selected' : '' }}>Unreleased</option>
+                <option value="cancelled" {{ request('status') == 'cancelled' ? 'selected' : '' }}>Cancelled</option>
+            </select>
+            <select name="rating" class="mb-4">
+                <option value="" {{ request('rating') == '' ? 'selected' : '' }}>All ratings</option>
+                <option value="all ages" {{ request('rating') == 'all ages' ? 'selected' : '' }}>All Ages</option>
+                <option value="kids" {{ request('rating') == 'kids' ? 'selected' : '' }}>Kids</option>
+                <option value="teens" {{ request('rating') == 'teens' ? 'selected' : '' }}>Teens</option>
+                <option value="adults" {{ request('rating') == 'adults' ? 'selected' : '' }}>Adults</option>
+            </select>
+            <select name="genre" class="mb-4">
+                <option value="" {{ request('genre') == '' ? 'selected' : '' }}>All genres</option>
+                @foreach(\App\Models\Genre::all() as $genre)
+                    <option value="{{ $genre->name }}" {{ request('genre') == $genre->name ? 'selected' : '' }}>
+                        {{ $genre->name }}
+                    </option>
+                @endforeach
+            </select>
+        </form>
         <div class="w-full max-w-4xl">
             <h2 class="text-2xl font-semibold mb-4">Movies</h2>
 
