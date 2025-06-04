9835fbb128e2cde98bce84d1e83e43f37ba49223
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2025-06-04 11:20:13 +0100

    Add Review Controller

---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+
+class ReviewController extends Controller
+{
+    public function movieReviews(Movie $movie)
+    {
+        $reviews = $movie->reviews()->paginate(3);
+
+        return view('movies.reviews', [
+            'reviews' => $reviews
+        ]);
+    }
+}
