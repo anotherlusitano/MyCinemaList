3623e252e8bc2565225fe10ef5389336b45bbfe8
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2025-06-26 10:35:03 +0100

    Add `remove` prop to delete-dialog component

--- resources/views/components/delete-dialog.blade.php
+++ resources/views/components/delete-dialog.blade.php
@@ -1,4 +1,9 @@
-@props(['name', 'route'])
+@props(['name', 'route', 'remove' => false])
+
+@php
+    // Sometimes its better to say "remove" instead of "delete"
+    $word = $remove ? 'remove' : 'delete';
+@endphp
 
 <div
     x-show="showModal"
@@ -19,10 +24,10 @@
 
         <div class="w-full flex flex-col items-center text-center">
             <x-gmdi-warning class="w-12 h-12 text-red-600"/>
-            <h2 class="text-lg font-semibold mb-4">Are you sure you want to delete {{ $name }}?</h2>
+            <h2 class="text-lg font-semibold mb-4">Are you sure you want to {{ $word . ' ' . $name }}?</h2>
             <button type="submit"
                     class="w-36 bg-red-500 text-white py-2 rounded hover:bg-red-600">
-                Yes, delete {{ $name }}
+                Yes, {{ $word . ' ' . $name }}
             </button>
         </div>
     </form>
