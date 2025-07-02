fdccced445e26510ea3d8af93325972ee4720ae8
João Ribeiro <joao.rosado.ribeiro@gmail.com>
2025-07-02 14:55:31 +0100

    Improve style of login and forget-password pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,14 +7,14 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="__('Email')" class="!text-gray-600" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Password')" class="!text-gray-600" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -27,21 +27,33 @@
         <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                <input id="remember_me" type="checkbox" class="rounded bg-gray-900 border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-600 focus:ring-offset-gray-800" name="remember">
+                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
+        <div class="flex items-center justify-between mt-4">
+            <div>
+                <p class="text-sm text-gray-800 rounded-md focus:outline-none">
+                    {{ __('Already have an account?') }}
+                </p>
+                <a href="{{ route('register') }}" class="underline text-sm text-gray-600 cursor-pointer hover:text-gray-900 rounded-md focus:outline-none">
+                    {{ __('Click here!') }}
                 </a>
-            @endif
+            </div>
 
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+            <div class="flex flex-col items-end gap-2">
+                <x-primary-button class="max-w-24 !text-center !bg-gray-800 !hover:bg-gray-700 !focus:bg-gray-700 !active:bg-gray-900 !text-white">
+                    {{ __('Log in') }}
+                </x-primary-button>
+
+                @if (Route::has('password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
+                @endif
+
+            </div>
         </div>
     </form>
 </x-guest-layout>
